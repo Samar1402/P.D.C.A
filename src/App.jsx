262f61f21c1330673073535559d701cc4978b506
Samarjeet <samarjeetpandey1402@gmail.com>
2024-12-27 22:07:01 +0530

    add navbar and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <>
       <Header />
       <Navbar />
+      <Home />
     </>
   );
 }
