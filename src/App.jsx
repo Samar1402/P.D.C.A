656d9f01dc0b994436fec5988a62ee93bda46366
abhi143x <iabhirajverma35@gmail.com>
2024-12-29 16:43:25 +0530

    Signup page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
+import Signup from "./Components/SignUp";
 
 function App() {
   return (
     <>
-      <Header />
-      <Navbar />
-      <Login />
-      <Home />
+      {/* <Header /> */}
+      {/* <Navbar /> */}
+      {/* <Login /> */}
+      {/* <Home /> */}
+      <Signup />
     </>
   );
 }
