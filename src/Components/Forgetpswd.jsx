39c2b725b91a1471688b4dfc0c24188b3bcffd25
Suraj Gupta <surajgupta512000@gmail.com>
2025-01-07 22:48:21 +0530

    Khesari Lal

--- src/Components/Forgetpswd.jsx
+++ src/Components/Forgetpswd.jsx
@@ -9,16 +9,24 @@ const ForgetPswd = ({ onNavigate, username }) => {
   const [confirmPassword, setConfirmPassword] = useState("");
 
   return (
-    <div className="flex items-center justify-center mt-4">
-      <div className="w-full max-w-md p-8 space-y-4 bg-blue-950 shadow-md rounded-lg">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-6 bg-blue-950 shadow-md rounded-lg">
         <div className="flex items-center justify-center space-x-4">
-          <h2 className="text-2xl font-bold text-center text-white">Forgot Password</h2>
-          <img src={cricketer} alt="Cricketer" className="w-11 animate-bounce" />
+          <h2 className="text-2xl font-bold text-center text-white">
+            Forgot Password
+          </h2>
+          <img
+            src={cricketer}
+            alt="Cricketer"
+            className="w-11 animate-bounce"
+          />
         </div>
 
-        <form>
-          <div className="mb-4">
-            <label className="block text-orange-500">Username</label>
+        <form className="space-y-6">
+          <div>
+            <label className="block mb-2 text-sm font-medium text-white">
+              Username
+            </label>
             <input
               type="text"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400"
@@ -27,8 +35,10 @@ const ForgetPswd = ({ onNavigate, username }) => {
               readOnly // Make the username field read-only
             />
           </div>
-          <div className="mb-4">
-            <label className="block text-orange-500">New Password</label>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-white">
+              New Password
+            </label>
             <input
               type="password"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400"
@@ -37,8 +47,10 @@ const ForgetPswd = ({ onNavigate, username }) => {
               onChange={(e) => setNewPassword(e.target.value)}
             />
           </div>
-          <div className="mb-4">
-            <label className="block text-orange-500">Confirm Password</label>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-white">
+              Confirm Password
+            </label>
             <input
               type="password"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400"
@@ -49,15 +61,19 @@ const ForgetPswd = ({ onNavigate, username }) => {
           </div>
           <button
             type="submit"
-            className="w-full flex items-center justify-center px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-green-700 transform hover:scale-105 transition-all duration-300"
+            className="w-full flex items-center justify-center px-4 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600 transform hover:scale-105 transition-all duration-300"
           >
-            <FontAwesomeIcon icon={faUnlockAlt} className="mr-2 animate-pulse" />
+            <FontAwesomeIcon
+              icon={faUnlockAlt}
+              className="mr-2 animate-pulse"
+            />
             Reset Password
           </button>
         </form>
-        <div className="flex justify-between text-sm text-green-600 mt-4">
-          <Link to="/login"
-            className="flex items-center hover:text-green-700 transition-all duration-300 cursor-pointer"
+        <div className="flex justify-between text-sm text-white mt-4">
+          <Link
+            to="/login"
+            className="flex items-center hover:text-red-700 transition-all duration-300 cursor-pointer"
           >
             <FontAwesomeIcon icon={faArrowLeft} className="mr-1 animate-bounce" />
             Back to Login
