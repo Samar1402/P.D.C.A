c2db897b23f786f907021fe0cabdbb4e2cfe9e61
Samarjeet <samarjeetpandey1402@gmail.com>
2025-01-10 22:36:32 +0530

    some updates

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -21,19 +21,19 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-blue-50 px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md p-8 space-y-6 bg-blue-900 rounded-lg shadow-lg">
-        <h2 className="flex items-center justify-center text-2xl sm:text-3xl font-bold text-white">
-          Sign Up
+    <div className="flex min-h-screen items-center justify-center bg-blue-50 p-4">
+      <div className="w-full max-w-md p-8 space-y-6 bg-blue-900 rounded-lg shadow-md">
+        <h2 className="flex flex-row items-center justify-center text-2xl font-bold text-center text-white">
+          Sign Up{" "}
           <img
             src={Cricketer}
             alt="logo"
-            className="h-14 sm:h-16 pb-2 ml-2 animate-bounce"
+            className="h-16 sm:h-20 pb-3 ml-2 animate-bounce"
           />
         </h2>
         <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
-            <label className="block text-sm sm:text-base font-medium text-white">
+            <label className="block text-l font-medium text-white">
               First Name
             </label>
             <input
@@ -46,7 +46,7 @@ const Signup = () => {
             />
           </div>
           <div>
-            <label className="block text-sm sm:text-base font-medium text-white">
+            <label className="block text-l font-medium text-white">
               Last Name
             </label>
             <input
@@ -59,9 +59,7 @@ const Signup = () => {
             />
           </div>
           <div>
-            <label className="block text-sm sm:text-base font-medium text-white">
-              Email
-            </label>
+            <label className="block text-l font-medium text-white">Email</label>
             <input
               type="email"
               name="email"
@@ -72,7 +70,7 @@ const Signup = () => {
             />
           </div>
           <div>
-            <label className="block text-sm sm:text-base font-medium text-white">
+            <label className="block text-l font-medium text-white">
               Contact No
             </label>
             <input
@@ -85,7 +83,7 @@ const Signup = () => {
             />
           </div>
           <div>
-            <label className="block text-sm sm:text-base font-medium text-white">
+            <label className="block text-l font-medium text-white">
               Password
             </label>
             <input
@@ -99,12 +97,12 @@ const Signup = () => {
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-md transform transition-transform hover:scale-105 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-md transform transition-transform hover:scale-105 hover:bg-blue-600"
           >
             Sign Up
           </button>
         </form>
-        <p className="text-sm sm:text-base text-center text-white">
+        <p className="text-m text-center text-white">
           Already have an account?{" "}
           <a href="/login" className="text-orange-500 hover:underline">
             Sign In
